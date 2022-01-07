5c87028867939aa6e48b3dcd487d7c34a49223d3
NuminousBlue <jdiehl2236@gmail.com>
2022-01-07 17:20:43 -0600

    added .env file to protect vars

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Initialize database connection
-mongoose.connect("mongodb://localhost/pennyPinch", {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
